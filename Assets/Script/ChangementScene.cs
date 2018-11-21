c074ea7f8a3e8a3dc56b6ea6f43a9373db904680
Isirda <isirda123@yahoo.fr>
2018-11-21 11:56:26 +0100

    Code Changement Scene

---
+++ Assets/Script/ChangementScene.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangementScene : MonoBehaviour {
+
+    GameObject[] NbOfRoom;
+    int QuelScene = 0;
+
+    // Use this for initialization
+    void Start() {
+        NbOfRoom = new GameObject[transform.childCount - 1];
+        int j = 0;
+        for (int i =0; i<this.transform.childCount; i++)
+        {
+            
+            if (transform.GetChild(i).gameObject.name.Contains("Test"))
+            {
+                NbOfRoom[j] = transform.GetChild(i).gameObject;
+                NbOfRoom[j].gameObject.SetActive(false);
+                j++;
+            }
+            
+        }
+        NbOfRoom[0].gameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangementSceneButton()
+    {
+        NbOfRoom[QuelScene].gameObject.SetActive(false);
+        QuelScene += 1;
+        if (QuelScene >= NbOfRoom.Length)
+        {
+            QuelScene = 0;
+        }
+        NbOfRoom[QuelScene].gameObject.SetActive(true);
+    }
+}
